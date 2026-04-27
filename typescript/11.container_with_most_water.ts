504bfd704764cffb862d6c6182d482e6bed536f9
luan-services <luandotservices@gmail.com>
2026-04-27 19:45:14 -0300

    refact: updated container_with_most_water_solutioN

--- typescript/11.container_with_most_water.ts
+++ typescript/11.container_with_most_water.ts
@@ -1,28 +1,25 @@
 function maxArea(height: number[]): number {
-    let arrLen = height.length;
-    let left = 0;
-    let right = arrLen - 1; 
-    let max = -1;
 
-    while (left != right) {
-        let leftLine = height[left]
-        let rightLine = height[right]
-        
-        if (rightLine >= leftLine) {
-            let sum = (right - left) * leftLine;
-            if (sum > max) {
-                max = sum;
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+
+    while (left < right) {
+        if (height[left] > height[right]) {
+            let water = (right - left) * height[right];
+            if (maxWater < water) {
+                maxWater = water;
             }
-            left += 1;
-        }
-        else if (leftLine > rightLine) {
-            let sum = (right - left) * rightLine;
-            if (sum > max) {
-                max = sum;
+            right --;
+        } else {
+            let water = (right - left) * height[left];
+            if (maxWater < water) {
+                maxWater = water;
             }
-            right -= 1;
+            left ++;
         }
     }
+
+    return maxWater;
     
-    return max;
 };
